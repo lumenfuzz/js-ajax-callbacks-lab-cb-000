b2355333876661fed060a11ece70f4b88e507066
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-17 21:56:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ function displayError() {
   document.getElementById("errors").innerHTML += "I'm sorry, there's been an error. Please try again."
 }
 
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
 function searchRepositories() {
   var query = document.getElementById("searchTerms").value
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", `https://github.com/search?utf8=✓&q=${query}`)
+  req.send()
 }
 
 function showCommits() {
