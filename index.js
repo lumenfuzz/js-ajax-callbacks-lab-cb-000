1d11722d64950b3068494f6ff3a1dfa4f086f27c
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-17 21:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
 });
 
 function displayError() {
-  document.getElementById("errors").innerHTML = "I'm sorry, there's been an error. Please try again."
+  document.getElementById("errors").innerHTML += "I'm sorry, there's been an error. Please try again."
 }
 
 function searchRepositories() {
